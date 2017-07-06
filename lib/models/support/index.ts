d2f0b0e5bb1d587cbb58058b2a3a9f87cc4a654e
Nas Villanueva <jvillanueva@exist.com>
2017-07-06 15:07:17 +0800

    Fixed typo in support models index.ts
    Fixed exports
    
    Fixed exports

--- lib/models/support/index.ts
+++ lib/models/support/index.ts
@@ -1,4 +1,5 @@
 export {default as CleengError} from './cleeng-error.model';
 export {default as CleengMessage} from './cleeng-message.model';
 export {default as CleengRequest} from './cleeng-request.model';
-export {default as CleengResult} from './cleeng-response.model';
+export {default as CleengResponse} from './cleeng-response.model';
+export {default as CleengListResponse} from './cleeng-list-response.model';
